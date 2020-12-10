acb4434cdddc7fac80e02521675eeef05b6a888f
dymafr <erwan1309@hotmail.com>
2020-12-10 14:25:17 +0100

    Initial commit

---
+++ src/app/color.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostListener, OnInit } from "@angular/core";
+
+@Directive({
+  selector: "[appColor]"
+})
+export class ColorDirective implements OnInit {
+  @HostListener("mouseenter", ["$event.target"]) private surligne(
+    e: MouseEvent
+  ) {
+    console.log(e);
+    this.el.nativeElement.style.color = "red";
+  }
+
+  @HostListener("mouseleave") private normal() {
+    this.el.nativeElement.style.color = "black";
+  }
+
+  @HostListener("window:click", ["$event.target"]) private test(e: MouseEvent) {
+    console.log(e);
+  }
+
+  constructor(private el: ElementRef<HTMLLIElement>) {}
+
+  ngOnInit() {}
+}
